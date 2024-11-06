d931a102b9fbbe26134cfafc4412ffb8a7fc218b
TMoiseyenko06 <tmoiseyenko06@gmail.com>
2024-11-06 13:13:13 -0500

    fixed general bugs and added working routes

--- Routes/authBP.py
+++ Routes/authBP.py
@@ -1,3 +1,5 @@
 from flask import Blueprint
+from Controllers import authController
 
 auth_blueprint = Blueprint('auth_blueprint',__name__)
+auth_blueprint.route('/register',methods=['POST'])(authController.register)
--- middleware/schemas/authSchemas.py
+++ middleware/schemas/authSchemas.py
@@ -1,4 +1,4 @@
-from finance_managment_app.backend.schema import ma
+from schema import ma
 from marshmallow import fields
 
 class User(ma.Schema):
