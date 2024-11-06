e97f36af6b50f600bb86bd2467077b485d17d55f
TMoiseyenko06 <tmoiseyenko06@gmail.com>
2024-11-06 14:55:34 -0500

    Expanded auth routes

--- Services/authServices.py
+++ Services/authServices.py
@@ -1,18 +1,30 @@
 import pymongo
 from werkzeug.security import generate_password_hash
+from flask import jsonify
+from utils.util import encode_token
 
 cluster = pymongo.MongoClient('mongodb://localhost:27017/')
 db = cluster['finance_db']
 accounts_collection = db['accounts']
 
 def register(user_data):
-    new_account = {
-        "username":user_data['username'],
-        "password":generate_password_hash(user_data['password'])
-    }
-    accounts_collection.insert_one(new_account)
+    if accounts_collection.count_documents({'username':user_data['username']}, limit = 1):
+        new_account = {
+            "username":user_data['username'],
+            "password":generate_password_hash(user_data['password'])
+        }
+        accounts_collection.insert_one(new_account)
+        return jsonify({"status":"OK",
+                        "message":"User Register"    
+                        }),201
+    else:
+        return jsonify({"error":"username already exists"})
 
 def login(user_data):
-    user = accounts_collection.find_one({"username":user_data['username'],"password":generate_password_hash(user_data['password'])})
-    if user:
-        print(user['_id'])
+    if accounts_collection.count_documents({"username":user_data['username']}, limit = 1 ):
+        user = accounts_collection.find({"username":user_data['username'],"password":generate_password_hash(user_data['password'])})
+        user_id = user['_id']
+        return jsonify({
+            "status":"OK",
+            "auth_token":encode_token(user_id)
+                        }),200
